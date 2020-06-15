7e12334faa1a6ab7975fd43f66dd7a80b18370e7
Arunim Joarder <44579152+ArunimJoarder@users.noreply.github.com>
2020-06-15 17:16:52 +0530

    added joint marker tracking

--- kinect_robo/src/move_arm.py
+++ kinect_robo/src/move_arm.py
@@ -1,34 +1,44 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import rospy
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 
 def move_arm():
     rospy.init_node('arm_controller_custom', anonymous=False)
-    topic_name = '/arm_controller/command'
+    
+    topic_name = 'custom/arm_controller/command'
     pub = rospy.Publisher(topic_name, JointTrajectory, queue_size=1)
-    trajectory=JointTrajectory()
-    point=JointTrajectoryPoint()
+
     rate = rospy.Rate(30)
     
-    trajectory.header.frame_id = "/base_link"
-
-    trajectory.joint_names.append("waist")
-    trajectory.joint_names.append("shoulder")
-    trajectory.joint_names.append("elbow")
-    trajectory.joint_names.append("wrist_angle")
-    trajectory.joint_names.append("wrist_rotate")
-
-    point.positions.append(1.0)
-    point.positions.append(0.3)
-    point.positions.append(0.5)
-    point.positions.append(0.6)
-    point.positions.append(0.5)
-    point.time_from_start.secs = rospy.Time.now().secs + 1
-
-    trajectory.points.append(point)
-
     while not rospy.is_shutdown():
+        trajectory=JointTrajectory()
+        point=JointTrajectoryPoint()
+        
+        trajectory.header.frame_id = "/base_link"
+
+        trajectory.joint_names.append("waist")
+        trajectory.joint_names.append("shoulder")
+        trajectory.joint_names.append("elbow")
+        trajectory.joint_names.append("wrist_angle")
+        trajectory.joint_names.append("wrist_rotate")
+
+        print('Enter Value for angles:')
+        a = float(input('  Waist:\t'))
+        b = float(input('  Shoulder:\t'))
+        c = float(input('  Elbow:\t'))
+        d = float(input('  Wrist Angle:\t'))
+        e = float(input('  Wrist Rotate:\t'))
+
+        point.positions.append(a)
+        point.positions.append(b)
+        point.positions.append(c)
+        point.positions.append(d)
+        point.positions.append(e)
+        point.time_from_start.secs = 1
+
+        trajectory.points.append(point)
+
         trajectory.header.stamp=rospy.Time.now()
 
         pub.publish(trajectory)
